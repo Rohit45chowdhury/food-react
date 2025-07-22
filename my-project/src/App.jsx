b2c0a59a341d29ce0e1736a6f57f85f2bce21c34
Your Name <your-email@example.com>
2025-07-22 20:36:40 +0530

    upload project

---
+++ my-project/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar';
+
+import Foods from './components/Foods';
+const App=()=>{
+  return (
+    <div>
+    <Navbar/>
+    <Foods/>
+    </div>
+    
+  )
+   
+}
+
+export default App;
